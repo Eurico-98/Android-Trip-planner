169492249b5b344dc3709ba406d49e1d3752b09c
ej <eurico2503@gmail.com>
2022-09-14 16:57:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projecto_cm/Function1_Activity.java
@@ -0,0 +1,14 @@
+package com.example.projecto_cm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Function1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function1);
+    }
+}
